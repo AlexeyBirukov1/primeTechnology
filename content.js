d960db98d55c27e676dab160fd145e4e001fa93b
MooNovu <apostoldemon15@mail.ru>
2025-03-12 13:57:25 +0700

    Расширение старт

---
+++ content.js
@@ -0,0 +1,7 @@
+let courseName = document.querySelector("format-price")?.innerText || "Название не найдено";
+let coursePrice = document.querySelector(".price")?.innerText || "Цена не найдена";
+let courseDescription = document.querySelector("p")?.innerText || "Описание не найдено";
+
+console.log("Курс:", courseName);
+console.log("Цена:", coursePrice);
+console.log("Описание:", courseDescription);
